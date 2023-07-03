cda48ea59b57e7658247a8b43773be0fd7fc1f23
kenta asakura <asakura.kenta963420@gmail.com>
2023-07-03 22:02:57 +0900

    Concept component made, need to pass data thru props

--- react-tutorial-key-concepts/src/App.js
+++ react-tutorial-key-concepts/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
+import ConceptItem from './ConceptItem';
 
-function App() {
+
+const App = () => {
+  const concepts = [
+    {
+      title: 'Components',
+      image: componentsImage,
+      description:
+        'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+    },
+    {
+      title: 'State',
+      image: stateImage,
+      description:
+        'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+    },
+    {
+      title: 'Events',
+      image: eventsImage,
+      description:
+        'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
       </header>
+      <ConceptItem
+        image={concepts[0].image}
+        title={concepts[0].title}
+        description={concepts[0].description}
+      />
     </div>
   );
 }
---
+++ react-tutorial-key-concepts/src/ConceptItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ConceptItem = ({ props }) => {
+  return (
+    <ul id="concepts">
+      <li className="concept">
+        <img src={props.image} alt="TODO: TITLE" />
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </li>
+    </ul>
+  );
+};
+
+export default ConceptItem;
