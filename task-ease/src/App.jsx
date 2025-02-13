c5adc5b9cb482a0e40d5f4cff13a5d036ac33a1d
kenta asakura <asakura.kenta963420@gmail.com>
2025-02-13 15:06:00 +0900

    NavBar Component imported and rendered in App Component, NavBar base styling and markup added

--- task-ease/src/App.jsx
+++ task-ease/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar'
 import KanbanPage from './pages/KanbanPage'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <div>
+      <NavBar />
       <KanbanPage></KanbanPage>
     </div>
   )
--- task-ease/src/components/NavBar.jsx
+++ task-ease/src/components/NavBar.jsx
@@ -1,8 +1,21 @@
 function NavBar() {
   return (
-    <div>
+    <header className="flex flex-row items-center min-h-[55px] md:min-h-[64px] px-4 md:px-6 bg-gray-700 shadow-sm">
+      <nav className="flex flex-row justify-between content-center w-full">
+        <button>
+          {/* <img src="" alt="" /> */}
+          Logo
+        </button>
 
-    </div>
+        <div>
+          <button>Add New Task</button>
+          <button>
+            {/* <img src="" alt="" /> */}
+            Edit Button
+          </button>
+        </div>
+      </nav>
+    </header>
   )
 }
 
