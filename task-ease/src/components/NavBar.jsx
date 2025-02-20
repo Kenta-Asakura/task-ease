d6930dd3269f6fa74d2b56ba007c7003c9c07d6a
kenta asakura <asakura.kenta963420@gmail.com>
2025-02-20 11:26:42 +0900

    NavBar main logo and buttons styling added

--- task-ease/src/components/NavBar.jsx
+++ task-ease/src/components/NavBar.jsx
@@ -1,17 +1,22 @@
+import logo from "../assets/task-ease.png"
+
 function NavBar() {
   return (
     <header className="fixed inset-0 flex z-[100] flex-row items-center h-[55px] md:h-[64px] px-4 md:px-6 bg-gray-700 shadow-sm">
-      <nav className="flex flex-row justify-between content-center w-full">
+      <nav className="flex flex-row justify-between content-center w-full text-gray-50">
         <button>
-          {/* <img src="" alt="" /> */}
-          Logo
+          <img src={logo}
+            alt="Task Ease Logo"
+            className="h-6 md:h-7"
+          />
         </button>
 
-        <div>
-          <button>Add New Task</button>
+        <div className="flex flex-row items-center gap-2">
+          <button className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full">Add New Task</button>
           <button>
-            {/* <img src="" alt="" /> */}
-            Edit Button
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
           </button>
         </div>
       </nav>
--- task-ease/src/pages/KanbanPage.jsx
+++ task-ease/src/pages/KanbanPage.jsx
@@ -2,7 +2,7 @@ import Board from "../components/Board";
 
 function KanbanPage() {
   return (
-    <div className="min-h-screen bg-gray-800 mt-[55px] md:mt-[64px]">
+    <div className="min-h-screen bg-gray-800 mt-[55px] md:mt-[64px] ">
       <Board />
     </div>
   )
