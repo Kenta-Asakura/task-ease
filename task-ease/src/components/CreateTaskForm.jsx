a4b6108dd427345f024ab0405c9a14b78cb5f7d7
kenta asakura <asakura.kenta963420@gmail.com>
2025-03-22 19:01:28 +0900

    Close CreateTaskForm when clicked outside added, component also refactored

--- task-ease/src/components/CreateTaskForm.jsx
+++ task-ease/src/components/CreateTaskForm.jsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid"; // Import UUID for unique IDs
+import { v4 as uuidv4 } from "uuid";
 
 function CreateTaskForm({ setTasks, setShowForm }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [status, setStatus] = useState("New");
-
-  // test
-  // console.log(typeof title);
-  // console.log('title - ', title);
-  // console.log(typeof description);
-  // console.log('description - ', description);
 
   console.log(setShowForm);
 
@@ -18,7 +12,7 @@ function CreateTaskForm({ setTasks, setShowForm }) {
     e.preventDefault();
 
     const newTask = {
-      id: uuidv4(), // generate a unique ID
+      id: uuidv4(),
       title,
       description,
       status
@@ -35,74 +29,79 @@ function CreateTaskForm({ setTasks, setShowForm }) {
     setShowForm(false);
   };
 
+  const handleOutsideClick = (e) => {
+    e.target.id === "form-overlay" && setShowForm(false);
+  };
+
   return (
-    <form
-      onSubmit={handleSubmit}
+    <div
+      id="form-overlay"
       className="fixed z-[500] inset-0
-        flex justify-center items-center
-        bg-black bg-opacity-30"
+      flex justify-center
+      bg-black bg-opacity-30"
+      onClick={handleOutsideClick}
     >
+      <form onSubmit={handleSubmit} className="flex items-center">
 
-      <div
-        className="editor
-          flex justify-center flex-col gap-y-4
-          mx-auto w-6/12
-          bg-gray-800 border-gray-700
-          text-gray-800 border p-4 shadow-lg max-w-2xl"
-      >
-        <input
-          className="title bg-gray-100 border border-gray-300 p-2 outline-none"
-          spellCheck="false"
-          placeholder="Title"
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-
-        <textarea
-          className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
-          spellCheck="false"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-
-        <select
-          className="bg-gray-100 border border-gray-300 p-2 mb-4 outline-none"
-          value={status}
-          onChange={(e) => setStatus(e.target.value)}
+        <div className="editor
+            flex justify-center flex-col gap-y-4
+            mx-auto p-4 max-w-2xl
+            bg-gray-800 border-gray-700
+            border shadow-lg text-gray-800 "
         >
-          <option value="New">New</option>
-          <option value="Now">Now</option>
-          <option value="Next">Next</option>
-          <option value="Done">Done</option>
-        </select>
+          <input
+            className="title bg-gray-100 border border-gray-300 p-2 outline-none"
+            spellCheck="false"
+            placeholder="Title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
 
-        <div className="buttons flex mt-4">
-          <button
-            type="button"
-            onClick={() => setShowForm(false)}
-            className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer
-                          text-gray-500 ml-auto transition duration-200
-                          hover:bg-gray-300 hover:text-gray-900"
-          >
-            Cancel
-          </button>
+          <textarea
+            className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
+            spellCheck="false"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
 
-          <button
-            type="submit"
-            className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer
-                          text-gray-200 ml-2 bg-indigo-500 transition duration-200
-                          hover:bg-indigo-600 hover:border-indigo-600"
+          <select className="bg-gray-100 border border-gray-300 p-2 mb-4 outline-none"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
           >
-            Post
-          </button>
+            <option value="New">New</option>
+            <option value="Now">Now</option>
+            <option value="Next">Next</option>
+            <option value="Done">Done</option>
+          </select>
+
+          <div className="buttons flex mt-4">
+            <button
+              type="button"
+              onClick={() => setShowForm(false)}
+              className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer
+                            text-gray-500 ml-auto transition duration-200
+                            hover:bg-gray-300 hover:text-gray-900"
+            >
+              Cancel
+            </button>
+
+            <button
+              type="submit"
+              className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer
+                            text-gray-200 ml-2 bg-indigo-500 transition duration-200
+                            hover:bg-indigo-600 hover:border-indigo-600"
+            >
+              Post
+            </button>
+          </div>
         </div>
-      </div>
 
-    </form>
+      </form>
+    </div>
   );
 }
 
