64e8c1ff834726815411a20ff4c0ec129ddb3a75
kenta asakura <asakura.kenta963420@gmail.com>
2025-03-26 11:03:52 +0900

    added tasks count per column

--- task-ease/src/components/Column.jsx
+++ task-ease/src/components/Column.jsx
@@ -6,7 +6,7 @@ function Column({ title, tasks }) {
 
   return (
     <div className="flex flex-col gap-4 min-w-[280px]">
-      <h3 className="text-base md:text-xl uppercase text-gray-300">{title}</h3>
+      <h3 className="text-base md:text-xl uppercase text-gray-300">{title} ({tasks.length})</h3>
 
       <div className="flex flex-col gap-6">
         {/* <Task heading="Test Title" description="Test Description" />
